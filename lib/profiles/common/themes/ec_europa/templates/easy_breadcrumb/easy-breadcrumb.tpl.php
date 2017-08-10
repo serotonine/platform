3e275a752a3d2a61da65c0d95dbe3b7db3a8ff54
Alex Verbruggen <verbruggenalex@gmail.com>
2017-08-10 13:43:35 -0700

    Try regular profiles folder

---
+++ lib/profiles/common/themes/ec_europa/templates/easy_breadcrumb/easy-breadcrumb.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<nav<?php print render($attributes); ?> aria-label="breadcrumbs">
+  <span class="ecl-sr-only"><?php print render($here); ?></span>
+  <ol class="ecl-breadcrumbs__segments-wrapper">
+    <?php print render($easy_breadcrumb); ?>
+  </ol>
+</nav>
---
+++ lib/profiles/common/themes/ec_europa/templates/logo/logo.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<a<?php print $attributes; ?>>
+    <span class="ecl-sr-only"><?php print $title; ?></span>
+</a>
